011a50cbef975f493102c96de365281168ca0240
Sandyna <sandynasj@icloud.com>
2018-08-12 09:45:46 -1200

    Project Smars Notary

---
+++ flow/app/src/main/java/com/example/sandynasandaire/flow/ItemListActivity.java
@@ -0,0 +1,14 @@
+package com.example.sandynasandaire.flow;
+
+import android.app.FragmentManager;
+
+/**
+ * Created by Sandyna Sandaire on 12/08/2018.
+ */
+
+class ItemListActivity {
+    public FragmentManager getSupportFragmentManager() {
+        FragmentManager supportFragmentManager;
+        return supportFragmentManager;
+    }
+}
